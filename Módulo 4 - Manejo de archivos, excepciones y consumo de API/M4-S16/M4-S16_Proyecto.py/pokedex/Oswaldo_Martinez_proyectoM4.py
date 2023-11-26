ab74e0a7a0cbc0fae0ad773cedff3a9455f8ff64
Osoraptor <97771856+Osoraptor@users.noreply.github.com>
2023-11-26 08:20:12 -0600

    Se agregan cambios a los files finales

--- Módulo 4 - Manejo de archivos, excepciones y consumo de API/M4-S16/M4-S16_Proyecto.py/pokedex/Oswaldo_Martinez_proyectoM4.py
+++ Módulo 4 - Manejo de archivos, excepciones y consumo de API/M4-S16/M4-S16_Proyecto.py/pokedex/Oswaldo_Martinez_proyectoM4.py
@@ -41,15 +41,28 @@ pokemon_dict = {
 
 }
 
+
 datos = respuesta.json()
 
 
 nombre = datos['name']
-#nombre = nombre.upper()
-#pokemon_dict['Nombre'].append(nombre)
+nombre = nombre.upper()
+pokemon_dict['Nombre'] = nombre
+
+
 
 peso = datos['weight']
+peso = str(peso)
+peso_format = peso[:(len(peso)-1)] + "." + peso[(len(peso)-1):] + ' kg'
+pokemon_dict['Peso'] = peso_format
+
+
 tamagno = datos['height']
+tamagno = str(tamagno)
+tamagno_format = tamagno[:(len(tamagno)-1)] + "." + tamagno[(len(tamagno)-1):] + ' metros'
+pokemon_dict['Tamaño'] = tamagno_format
+
+
 
 
 movimientos = datos['moves']
@@ -73,16 +86,19 @@ for type in range(int(len(tipos))): # se hace in for por el rango de la cantidad
     pokemon_dict['Tipos'].append(tipo)
 
 
-print(nombre)
-print(peso)
-print(tamagno)
+
+### Validaciones print ###
+
+# print(nombre)
+# print(peso)
+# print(tamagno)
+
+################################
 
 
 print(pokemon_dict)
 
-#print(movimientos)
-# print(habilidades)
-# print(tipos)
+
 
 
 
@@ -91,25 +107,33 @@ print(pokemon_dict)
 
 
 
-# with open('agenda.txt', 'w') as f_agenda:
-#     for persona in personas:
-#         f_agenda.write(f' {persona[0]}. Nombre: {persona[1]} {persona[2]}. Edad: {persona[3]}. Correo: {persona[4]}. Telefono: {persona[5]}.\n')
-#     print('Datos guardados en agenda.txt.')
-#     #break
+with open('pokemon.json', 'w') as f_pokemon:
+    json.dump(pokemon_dict, f_pokemon)
 
+
+with open('pokemon.json', 'r') as f_pokemon:
+    data = json.load(f_pokemon)
+
+# Validación de lectura de la data del JSON file.
+
+print(f'''\n***
+\n {data} \n*** 
+''')
+
+#print(data)
 
 
 
 ############ IMAGEN ###########
 
 
-# try :
-#     url_imagen = datos['sprites']['front_default']
-#     imagen = Image.open(urlopen(url_imagen))
-# except:
-#     print('El pokémon no tiene imagen.')
-#     exit()
+try :
+    url_imagen = datos['sprites']['front_default']
+    imagen = Image.open(urlopen(url_imagen))
+except:
+    print('El pokémon no tiene imagen.')
+    exit()
 
-# plt.title(datos['name']) # para acceder a su nombre
-# imgplot = plt.imshow(imagen)
-# plt.show() 
+plt.title(datos['name']) # para acceder a su nombre
+imgplot = plt.imshow(imagen)
+plt.show() 
